2f34c8ad1112b5e2191337347f91802b59cebd49
Po Wing LEE <leepowing@gmail.com>
2026-07-12 03:53:59 +0800

    First version of PW Learning Hub

---
+++ app/page.tsx
@@ -0,0 +1,40 @@
+export default function Home() {
+  return (
+    <main
+      style={{
+        fontFamily: "Arial",
+        textAlign: "center",
+        padding: "60px",
+      }}
+    >
+      <h1>🎓 PW Learning Hub</h1>
+
+      <h2>Welcome!</h2>
+
+      <p>
+        This website is being built for Greta & Mathis.
+      </p>
+
+      <hr />
+
+      <h3>Choose a subject</h3>
+
+      <button>📚 Spelling</button>
+
+      <br />
+      <br />
+
+      <button>📖 Reading</button>
+
+      <br />
+      <br />
+
+      <button>✏️ Grammar</button>
+
+      <br />
+      <br />
+
+      <button>➗ Maths</button>
+    </main>
+  );
+}
